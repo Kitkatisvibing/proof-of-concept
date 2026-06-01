394735589c028ef2e470a7e5b5086acd8f224927
Kitkatisvibing <kate.jadi@gmail.com>
2026-06-01 12:02:47 +0200

    Server.js setup close #1

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Server setup
+
+import express from 'express'
+import { Liquid } from 'liquidjs';
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+app.set('views', './views')
+
+
+// Hier beginnen de views
+app.get('/', async function (request, response) {
+
+  response.render('index.liquid')
+})
+
+// Localhost setup
+
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`http://localhost:${app.get('port')}/ Pokemon\n\nGotta catch 'em all!`)
+})
+
