78b13fb9910fe3b0dc095d3039ff1356acb98145
Kitkatisvibing <kate.jadi@gmail.com>
2026-06-03 11:15:01 +0200

    render data in pokedex #3

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ app.set('views', './views')
 // Hier beginnen de views
 app.get('/', async function (request, response) {
 
-  response.render('index.liquid')
-})
+  const type = request.query.pokemon
 
+  const pokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon')
+
+  const pokemonResponseJSON = await pokemonResponse.json()
+
+  response.render('index.liquid', {
+    pokemon: pokemonResponseJSON,
+  })
+})
 // Localhost setup
 
 app.set('port', process.env.PORT || 8000)
